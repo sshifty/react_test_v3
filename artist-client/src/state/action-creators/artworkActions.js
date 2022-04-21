0c036e1e3f38f109cd6a5db5fd33f98e9bbc041f
sshifty <atiszon@gmail.com>
2022-04-21 11:52:04 +0200

    refactor: change artworklist url

--- artist-client/src/state/action-creators/artworkActions.js
+++ artist-client/src/state/action-creators/artworkActions.js
@@ -4,7 +4,7 @@ export const getArtworks = (page = 1, pageLimit = 25) => {
   return async (dispatch, getState) => {
     try {
       const resp = await fetch(
-        `https://api.artic.edu/api/v1/artists?page=${page}&limit=${pageLimit}`
+        `https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,?page=${page}&limit=${pageLimit}`
       );
       const data = await resp.json();
       return dispatch({
--- artist-client/src/state/reducers/artworkReducer.js
+++ artist-client/src/state/reducers/artworkReducer.js
@@ -1,12 +1,20 @@
 import { ARTWORK_FETCHALL } from "../typeConstants";
 
-const artworkReducer=(state=null,action)=>{
-    switch(action.type){
-        case ARTWORK_FETCHALL:
-            return action.payload;
-        default:
-            return state;
-    }
-}
+const artworkReducer = (state = null, action) => {
+  switch (action.type) {
+    case ARTWORK_FETCHALL:
+      return {
+        ...action.payload,
+        data: action.payload.data.map((item) => {
+          return {
+            ...item,
+            imageURL: `https://www.artic.edu/iiif/2/${item.image_id}/full/843,/0/default.jpg`,
+          };
+        }),
+      };
+    default:
+      return state;
+  }
+};
 
 export default artworkReducer;
