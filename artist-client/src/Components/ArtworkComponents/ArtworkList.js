1363b1a10b74c09d9d9f57bef933d8f3260176da
sshifty <atiszon@gmail.com>
2022-04-22 11:58:51 +0200

    feat: add control components for search/itemperpage

--- artist-client/src/Components/ArtworkComponents/ArtworkList.js
+++ artist-client/src/Components/ArtworkComponents/ArtworkList.js
@@ -5,50 +5,79 @@ import { artworkActionsCreators } from "../../state/action-creators";
 import ArtworkDetail from "./ArtworkDetail";
 import ReactPaginate from "react-paginate";
 import styles from "./ArtworkList.module.css";
+import Controls from "../Controls/Controls";
 import Paginate from "./Paginate.css";
 
 const ArtworkList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageCount, setPageCount] = useState(0);
-  const [searchValue, setSearchValue] = useState("");
+  const [controlState, setControlState] = useState({
+    searchValue: "",
+    itemsPerPage: 25,
+  });
   //fix this
-  const itemsPerPage = 25;
+
   const artworks = useSelector((state) => state.artworks, shallowEqual);
 
   const dispatch = useDispatch();
-  const { getArtworks,getQueryArtwork } = bindActionCreators(artworkActionsCreators, dispatch);
+  const { getArtworks, getQueryArtwork } = bindActionCreators(
+    artworkActionsCreators,
+    dispatch
+  );
 
   const setUsersOnPageChange = () => {
     setPageCount(
-      Math.ceil(artworks ? artworks.pagination.total_pages : 1 / itemsPerPage)
+      Math.ceil(
+        artworks
+          ? artworks.pagination.total_pages
+          : 1 / controlState.itemsPerPage
+      )
     );
   };
   // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     setCurrentPage(event.selected + 1);
   };
-  const handleChange = (e) => {
-    setSearchValue(e.target.value);
+
+  const onSearch = () => {
+    if (controlState.searchValue) {
+      getQueryArtwork(controlState.searchValue, 1, controlState.itemsPerPage);
+    } else {
+      getArtworks(currentPage, controlState.itemsPerPage);
+    }
   };
-  const onSubmit=e=>{
-    e.preventDefault();   
-    getQueryArtwork(searchValue);
-  }
+  const handleChange = (e) => {
+    setControlState((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
   useEffect(() => {
     getArtworks();
   }, []);
   useEffect(() => {
     setUsersOnPageChange();
-  }, [itemsPerPage, artworks]);
+  }, [controlState.itemsPerPage, artworks]);
   useEffect(() => {
-    getArtworks(currentPage);
-  }, [currentPage]);
+    if (controlState.searchValue) {
+      getQueryArtwork(
+        controlState.searchValue,
+        currentPage,
+        controlState.itemsPerPage
+      );
+    } else {
+      getArtworks(currentPage, controlState.itemsPerPage);
+    }
+  }, [currentPage, controlState.itemsPerPage]);
   return (
     <div className={styles.artworkListContainer}>
-      <form onSubmit={onSubmit}>
-        <input type="text"  onChange={handleChange
-        }/>
-      </form>
+      <Controls
+        handleChange={handleChange}
+        onSearch={onSearch}
+        itemsPerPage={controlState.itemsPerPage}
+      />
       <div className={styles.artworkListDetails}>
         {artworks
           ? artworks.data.map((item) => {
--- artist-client/src/Components/Controls/Controls.js
+++ artist-client/src/Components/Controls/Controls.js
@@ -1,15 +1,28 @@
 import styles from "./Controls.module.css";
 
-const Controls = () => {
+const Controls = ({ handleChange, onSearch, itemsPerPage }) => {
+  const array = [];
+  for (let i = 5; i <= 100; i += 5) {
+    array.push(i);
+  }
+
   return (
     <div className={styles.controlsContainer}>
       <div className={styles.inputContainer}>
-
-        <input type="text" />
+        <input onChange={handleChange} name="searchValue" type="text" />
       </div>
       <div className={styles.inputContainer}>
-        <input type="number" placeholder="xd"/>
+        <select onChange={handleChange} name="itemsPerPage" id="itemsPerPage">
+          {array.map((i) => {
+            return (
+              <option value={i} selected={itemsPerPage === i ? "selected" : ""}>
+                {i}
+              </option>
+            );
+          })}
+        </select>
       </div>
+      <button onClick={onSearch}>search</button>
     </div>
   );
 };
